6dc6817988a8469e57356a00e60ecab5359dff15
Bektemir <89856328+BektemirSatybaldiev@users.noreply.github.com>
2023-01-04 14:26:03 +0600

    Add files via upload
    Now files are ready

---
+++ service/clients/models.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Client(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    company_name = models.CharField(max_length=100)
+    full_adress = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"User - {self.user}"
+
+
