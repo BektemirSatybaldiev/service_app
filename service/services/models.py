610754eba4724ae777aeee555aaed336c77d7c5d
Bektemir <bektemirsatybaldiev@gmail.com>
2023-01-09 18:45:27 +0600

    Fixed Celery task + flower

--- service/services/models.py
+++ service/services/models.py
@@ -1,12 +1,22 @@
 from django.core.validators import MaxValueValidator
 from django.db import models
-
+from services.tasks import set_price
 from clients.models import Client
 
 
 class Service(models.Model):
     name = models.CharField(max_length=100)
     full_price = models.PositiveIntegerField()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__full_price = self.full_price
+
+    def save(self, *args, **kwargs):
+        if self.full_price != self.__full_price:
+            for subscription in self.subscriptions.all():
+                set_price.delay(subscription.id)
+        return super().save(*args, **kwargs)
 
     def __str__(self):
         return f"Service - {self.name}"
@@ -25,15 +35,25 @@ class Plan(models.Model):
                                                        MaxValueValidator(100)
                                                    ])
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__discount_percent = self.discount_percent
+
+    def save(self, *args, **kwargs):
+        if self.discount_percent != self.__discount_percent:
+            for subscription in self.subscriptions.all():
+                set_price.delay(subscription.id)
+        return super().save(*args, **kwargs)
+
     def __str__(self):
         return f"Plan type - {self.plan_type}, Discount percent- {self.discount_percent}"
-
 
 
 class Subscription(models.Model):
     client = models.ForeignKey(Client, related_name='subscriptions', on_delete=models.PROTECT)
     service = models.ForeignKey(Service, related_name='subscriptions', on_delete=models.PROTECT)
     plan = models.ForeignKey(Plan, related_name='subscriptions', on_delete=models.PROTECT)
+    price = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return f"{self.client}, {self.service}, {self.plan}"
--- service/services/views.py
+++ service/services/views.py
@@ -1,7 +1,5 @@
-from django.db.models import Prefetch, F, Sum
-from django.shortcuts import render
+from django.db.models import Prefetch, Sum
 from rest_framework.viewsets import ReadOnlyModelViewSet
-
 from clients.models import Client
 from services.models import Subscription
 from services.serializers import SubscriptionSerializer
@@ -12,8 +10,7 @@ class SubscriptionView(ReadOnlyModelViewSet):
         'plan',
         Prefetch('client', queryset=Client.objects.all().select_related('user').only('company_name',
                                                                                      'user__email'))
-    ).annotate(price=F('service__full_price') - F('service__full_price') *
-                     F('plan__discount_percent') / 100.00)
+    )
     serializer_class = SubscriptionSerializer
 
     def list(self, request, *args, **kwargs):
