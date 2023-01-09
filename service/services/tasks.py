1ded4bf69c486cebb0285d488fc0b1230b86d660
Bektemir <bektemirsatybaldiev@gmail.com>
2023-01-09 23:09:14 +0600

    Fixed 2 workers of Celery. Used transaction to maintain them

--- service/services/tasks.py
+++ service/services/tasks.py
@@ -1,13 +1,31 @@
+import datetime
+import time
+
 from celery import shared_task
+from django.db import transaction
 from django.db.models import F
+from celery_singleton import Singleton
 
 
-@shared_task
+@shared_task(base=Singleton)
 def set_price(subscription_id):
     from services.models import Subscription
 
-    subscription = Subscription.objects.filter(id=subscription_id).annotate(
-        annotated_price=F('service__full_price') -
-              F('service__full_price') * F('plan__discount_percent') / 100.00).first()
-    subscription.price = subscription.annotated_price
-    subscription.save()
+    with transaction.atomic():
+
+        subscription = Subscription.objects.select_for_update().filter(id=subscription_id).annotate(
+            annotated_price=F('service__full_price') -
+                        F('service__full_price') * F('plan__discount_percent') / 100.00).first()
+
+        subscription.price = subscription.annotated_price
+        subscription.save()
+
+
+@shared_task(base=Singleton)
+def set_comment(subscription_id):
+    from services.models import Subscription
+    with transaction.atomic():
+        subscription = Subscription.objects.select_for_update().get(id=subscription_id)
+
+        subscription.comment = str(datetime.datetime.now())
+        subscription.save()
